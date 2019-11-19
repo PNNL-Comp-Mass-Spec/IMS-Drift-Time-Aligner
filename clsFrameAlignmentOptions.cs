5515ec707ec1170d18d7f82068b3b266e9ad0861
alchemistmatt <matthew.monroe@pnnl.gov>
2019-11-18 18:26:27 -0800

    Updated PRISM.dll that supports argument -ParamFile

--- clsFrameAlignmentOptions.cs
+++ clsFrameAlignmentOptions.cs
@@ -12,7 +12,7 @@ namespace IMSDriftTimeAligner
         /// <summary>
         /// Program date
         /// </summary>
-        public const string PROGRAM_DATE = "November 11, 2019";
+        public const string PROGRAM_DATE = "November 13, 2019";
 
         /// <summary>
         /// Default frame selection mode
