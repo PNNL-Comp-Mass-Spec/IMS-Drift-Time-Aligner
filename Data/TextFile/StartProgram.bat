6a0dad21143088d337b5fa24f7d1e357f091be12
alchemistmatt <matthew.monroe@pnnl.gov>
2020-09-16 09:26:53 -0700

    Add example input text files

---
+++ Data/TextFile/StartProgram.bat
@@ -0,0 +1,9 @@
+
+..\..\Bin\IMSDriftTimeAligner.exe AlignmentTestData1.txt /conf:LinearRegressionOptions.conf /output:LinearRegression\Placeholder.txt
+
+..\..\Bin\IMSDriftTimeAligner.exe AlignmentTestData1.txt /conf:DTWOptions_ITF_10pct.conf /output:DTW_ITF_10pct\Placeholder.txt
+
+..\..\Bin\IMSDriftTimeAligner.exe AlignmentTestData1.txt /conf:DTWOptions_ITF_15pct.conf /output:DTW_ITF_15pct\Placeholder.txt
+
+pause
+
